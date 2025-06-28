b3c54aca4bef1d658b3ddeeea47f2581ab275a1f
malbuffer4pt <icbluewhale@gmail.com>
2025-06-28 23:41:48 +0800

    update

--- pyWinSrv/clsUser.cs
+++ pyWinSrv/clsUser.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +9,16 @@ namespace pyWinSrv
 {
     internal class clsUser
     {
+        public Socket m_sktUser;
+
+        public clsUser(Socket sktUser)
+        {
+            m_sktUser = sktUser;
+        }
+
+        public clsUser()
+        {
+
+        }
     }
 }
--- pyWinSrv/clsVictim.cs
+++ pyWinSrv/clsVictim.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +9,18 @@ namespace pyWinSrv
 {
     internal class clsVictim
     {
+        public Socket m_sktVictim;
+
+        public string m_szOnlineID;
+
+        public clsVictim(Socket socket)
+        {
+            m_sktVictim = socket;
+        }
+
+        public clsVictim()
+        {
+
+        }
     }
 }
